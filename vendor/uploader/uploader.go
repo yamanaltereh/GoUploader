c81baa8164994cdde015d2db78c2519aa71f3861
Yaman ALTareh <yamanaltareh@gmail.com>
2018-07-28 21:46:47 +0800

    fix uploader package

--- vendor/uploader/uploader.go
+++ vendor/uploader/uploader.go
@@ -7,7 +7,7 @@ import (
   "github.com/aws/aws-sdk-go/aws/session"
   "github.com/aws/aws-sdk-go/aws"
   "github.com/aws/aws-sdk-go/aws/credentials"
-  "github.com/aws/aws-sdk-go/service/s3"
+  // "github.com/aws/aws-sdk-go/service/s3"
   "github.com/aws/aws-sdk-go/service/s3/s3manager"
   "log"
 )
@@ -21,57 +21,57 @@ func loadAwsCredential() (access_key_id string, secret_access_key string, region
   bucket = os.Getenv("S3_BUCKET")
   access_key_id = os.Getenv("AWS_ACCESS_KEY_ID")
   secret_access_key = os.Getenv("AWS_SECRET_ACCESS_KEY")
-  region = os.Getenv("ap-southeast-1")
+  region = os.Getenv("region")
 
   return
 }
 
-func UploadMultipart(file multipart.File) {
-  fmt.Println("Multipart Uploader")
-  awsID, awsSecretKey, awsRegion, s3Bucket := loadAwsCredential()
-  token := ""
+// func UploadMultipart(file multipart.File) {
+//   fmt.Println("Multipart Uploader")
+//   awsID, awsSecretKey, awsRegion, s3Bucket := loadAwsCredential()
+//   token := ""
 
-  creds := credentials.NewStaticCredentials(awsID, awsSecretKey, token)
+//   creds := credentials.NewStaticCredentials(awsID, awsSecretKey, token)
 
-  // check credential validity
-  _, err := creds.Get()
-  if err != nil {
-    // handle error
-  }
+//   // check credential validity
+//   _, err := creds.Get()
+//   if err != nil {
+//     // handle error
+//   }
 
-  cfg := aws.NewConfig().WithRegion(awsRegion).WithCredentials(creds)
+//   cfg := aws.NewConfig().WithRegion(awsRegion).WithCredentials(creds)
 
-  svc := s3.New(session.New(), cfg)
+//   svc := s3.New(session.New(), cfg)
 
-  fileInfo, _ := file.Stat()
+//   fileInfo, _ := file.Stat()
 
-  size := fileInfo.Size()
+//   size := fileInfo.Size()
 
-  buffer := make([]byte, size) // read file content to buffer
+//   buffer := make([]byte, size) // read file content to buffer
 
-  file.Read(buffer)
+//   file.Read(buffer)
 
-  fileBytes := bytes.NewReader(buffer)
-  fileType := http.DetectContentType(buffer)
-  path := "/poc/" + file.Name()
+//   fileBytes := bytes.NewReader(buffer)
+//   fileType := http.DetectContentType(buffer)
+//   path := "/poc/" + file.Name()
 
-  params := &s3.PutObjectInput{
-    Bucket: s3Bucket,
-    Key: awsSecretKey,
-    Body: fileBytes,
-    ContentLength: aws.Int64(size),
-    ContentType: aws.String(fileType),
-  }
+//   params := &s3.PutObjectInput{
+//     Bucket: s3Bucket,
+//     Key: awsSecretKey,
+//     Body: fileBytes,
+//     ContentLength: aws.Int64(size),
+//     ContentType: aws.String(fileType),
+//   }
 
-  result, err := svc.PutObject(params)
-  if err != nil {
-    // handle error
-  }
+//   result, err := svc.PutObject(params)
+//   if err != nil {
+//     // handle error
+//   }
 
-  fmt.Printf("response %s", awsutil.StringValue(result))
-}
+//   fmt.Printf("response %s", awsutil.StringValue(result))
+// }
 
-func Upload(filepath string) {
+func Upload(filepath string, filename string, file_url string) {
   fmt.Println("Uploader")
 
   awsID, awsSecretKey, awsRegion, s3Bucket := loadAwsCredential()
@@ -90,9 +90,10 @@ func Upload(filepath string) {
 
   svc := s3manager.NewUploader(sess)
   fmt.Println("Uploading file to S3...")
+  prefix := os.Getenv("prefix") + filename
   result, err := svc.Upload(&s3manager.UploadInput{
     Bucket: aws.String(s3Bucket),
-    Key:    aws.String("filepath"),
+    Key:    aws.String(prefix),
     Body:   file,
   })
   if err != nil {
@@ -100,5 +101,7 @@ func Upload(filepath string) {
     os.Exit(1)
   }
 
-  fmt.Printf("Successfully uploaded %s to %s\n", "go_uploader_filename", result.Location)
+  file_url = result.Location
+  fmt.Printf("Successfully uploaded %s to %s\n", "go_uploader_filename", file_url)
+  return
 }
